87685368493c429060bc9a355b07b5242abf903c
mikolaj657 <161924077+mikolaj657@users.noreply.github.com>
2024-05-28 20:19:19 +0200

    Create Company.java

---
+++ Company.java
@@ -0,0 +1,22 @@
+import employees.Employee;
+import employees.Manager;
+import employees.Worker;
+
+public class Company {
+    public static void main(String[] args) {
+        Employee[] employees = new Employee[5];
+
+        // Dodawanie pracowników
+        employees[0] = new Manager("Mikołaj Kubicki", 50000, 0);
+        employees[1] = new Worker("Kacper Gabrysiak", 60000, "Deweloper");
+        employees[2] = new Employee("Wiktoria Pospolita", 55000);
+        employees[3] = new Worker("Anna Kadaj", 65000, "Księgowa");
+        employees[4] = new Worker("Piotr Nowak", 70000, "Menedżer marketingu");
+
+        // Wyświetlanie danych dla wszystkich pracowników
+        System.out.println("Dane dla wszystkich pracowników:");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+    }
+}
