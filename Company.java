5ac47a742c65528ab2dc9c01bd6cc8104034fbbd
mikolaj657 <161924077+mikolaj657@users.noreply.github.com>
2024-05-30 10:01:35 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -4,17 +4,28 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
+        Employee[] employees = new Employee[7];
 
-        // Dodawanie pracowników
+       // Dodawanie pracowników
         employees[0] = new Manager("Mikołaj Kubicki", 50000, 0);
         employees[1] = new Worker("Kacper Gabrysiak", 60000, "Deweloper");
         employees[2] = new Employee("Wiktoria Pospolita", 55000);
         employees[3] = new Worker("Anna Kadaj", 65000, "Księgowa");
-        employees[4] = new Worker("Piotr Nowak", 70000, "Menedżer marketingu");
+        employees[4] = new Worker("Piotr Nowak", 70000, "Menedżer marketingu")
+        employees[5] = new Manager("Daniel Syk", 60000, 0);
+        employees[6] = new Worker("Piotr Maj", 55000, "Specjalista HR");
 
-        // Wyświetlanie danych dla wszystkich pracowników
-        System.out.println("Dane dla wszystkich pracowników:");
+        // Aktualizacja wartości salary
+        for (Employee employee : employees) {
+            employee.setSalary(employee.getSalary() + 500);
+            if (employee instanceof Manager) {
+                ((Manager) employee).setNumberOfSubordinates(2); // Ustawienie liczby podwładnych
+                employee.setSalary(75000); // Ustawienie nowej pensji dla managerów
+            }
+        }
+
+        // Wyświetlanie zaktualizowanych danych o pracownikach
+        System.out.println("Zaktualizowane dane pracowników:");
         for (Employee employee : employees) {
             System.out.println(employee);
         }
